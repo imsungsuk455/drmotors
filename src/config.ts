8aec5d59f5a17944d71cd2ce6198e8adee910942
imsungsuk455 <imsungsuk455@users.noreply.github.com>
2026-04-10 22:01:54 +0900

    Update website URL to https://doctormotors.org/

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  website: "https://doctornomad.org/", // replace this with your deployed domain
+  website: "https://doctormotors.org/", // replace this with your deployed domain
   author: "닥터모터스(Dr. Motors)",
   profile: "/",
   desc: "자동차 최신 뉴스, 시승기, 관리 팁 등 자동차에 관한 모든 정보를 전문가의 시선으로 깔끔하게 정리해 드립니다. 당신의 카 라이프를 위한 완벽한 가이드.",
